345ca07bda020eb292dd396383835493013d7879
Kdkobie <kdkobie@gmail.com>
2015-04-27 10:15:31 -0700

    Term Blueprint
    Addressed #4

---
+++ site/blueprints/term.php
@@ -0,0 +1,23 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Term
+pages: event
+files: true
+fields:
+  title:
+    label: Term Name
+    type:  text
+  term:
+    label: Term
+    type: select
+    options:
+      - Fall
+      - Winter
+      - Spring
+      - Summer
+  classes_begin:
+    label: Classes Begin
+    type: date
+  classes_end:
+    label: Classes End
+    type: date
