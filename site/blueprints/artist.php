bfabaea7ede88daf46d4d85288acf3d88cc79a30
Kdkobie <kdkobie@gmail.com>
2015-04-27 10:41:12 -0700

    Artist Blueprint
    Addressed #6

---
+++ site/blueprints/artist.php
@@ -0,0 +1,17 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Artist
+pages: 
+  hide: true
+files: true
+fields:
+  title:
+    label: Full Name
+    type:  text
+  bio:
+    label: Artist's Bio
+    type: textarea
+  link:
+    label: Link
+    type: url
+  
