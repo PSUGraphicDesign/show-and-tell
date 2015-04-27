0a71a8b1c76386e41d8d4a042e400de334e6d566
Kdkobie <kdkobie@gmail.com>
2015-04-27 11:53:36 -0700

    Home Template
    Added new page for home page

---
+++ site/templates/home.php
@@ -0,0 +1,26 @@
+<? snippet('header') ?>
+<main>
+  <section>
+    <article>
+      <div class="column full">
+<h1><?= $page->title()->html() ?></h1>
+      </div>
+    </article>
+  </section>
+
+  <section class="artists">
+    <article>
+<? foreach ( page('artists')->children() as $artist ) { ?>
+      <div class="column third artist">
+        <h2>
+          <a href="<?= $artist->link() ?>"><?= $artist->title() ?></a>
+        </h2> 
+        <?= $artist->bio()->kirbytext() ?>
+       </div>
+      <? } ?>
+    </article>
+  </section>
+
+</main>
+
+<? snippet('footer') ?>
