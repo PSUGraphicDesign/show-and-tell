e0e4e581e94d4f8f254ba8cc27539ecd4a6557c4
August Miller <hello@gusmiller.com>
2015-05-11 11:40:43 -0700

    New controller variables for about page and departments

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -1,6 +1,8 @@
 <? return function ($site, $pages, $page) {
   return [
     'current_term' => ST::current_year()->current_term(),
-    'next' => ST::next_event()
+    'next' => ST::next_event(),
+    'about' => page('about'),
+    'departments' => page('about')->department_logos()->yaml()
   ];
 };
