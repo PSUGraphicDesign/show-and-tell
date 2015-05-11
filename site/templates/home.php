7bc0c41cf7acefa0aa90ca964d57b19055902a53
August Miller <hello@gusmiller.com>
2015-05-11 11:42:17 -0700

    Homepage updates
    - Background image location
    - Speaker bio in intro (Upcoming event)
    - Added About section
    - Added poster to current term schedule
    - Added dates to term’s events

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,25 +3,54 @@
 <main>
 
   <? if ( $next ) { ?>
-    <section class="upcoming">
+    <section class="upcoming" style="background-image: url(<?= $next->poster()->url() ?>)">
       <article>
-        <div class="column full <?= $next->lightness() ?>" style="background-image: url(<?= $next->poster()->url() ?>)">
-          <?= html::a($next->url(), $next->title()) ?>
+        <div class="column third <?= $next->lightness() ?>">
+          <h2><?= html::a($next->url(), $next->title()) ?></h2>
+          <?= $next->speaker()->bio()->kirbytext() ?>
         </div>
       </article>
     </section>
   <? } ?>
 
+  <section class="about">
+    <article class="header">
+      <div class="column full">
+        About
+      </div>
+    </article>
+    <article class="info">
+      <div class="column full">
+        <?= $about->text()->kirbytext() ?>
+      </div>
+    </article>
+    <article class="extra">
+      <div class="column quarter address">
+        <?= $about->details()->kirbytext() ?>
+      </div>
+      <div class="column three-quarters department-logos">
+        <? foreach ( $departments as $department ) { ?>
+          <?= html::img($about->image($department['logo'])->url()) ?>
+        <? } ?>
+      </div>
+    </article>
+  </section>
+
   <section class="this-term">
-    <article>
+    <article class="header">
       <div class="column half">
         <h2>This Term</h2>
+      </div>
+    </article>
+    <article class="event-list">
+      <div class="column half">
+        <?= html::img($current_term->image($current_term->poster())->url()) ?>
       </div>
       <div class="column half">
         <? if ( $current_term->count() ) { ?>
           <ul>
             <? foreach ( $current_term->events() as $event ) { ?>
-              <li><?= $event->time_sensitive_link() ?></li>
+              <li><?= $event->date('F jS') ?>: <?= $event->time_sensitive_link() ?></li>
             <? } ?>
           </ul>
         <? } else { ?>
