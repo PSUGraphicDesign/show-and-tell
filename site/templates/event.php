bd94cd8274f933f7c0b7bae9f9e8e4b358172a95
Kdkobie <kdkobie@gmail.com>
2015-05-27 09:38:15 -0700

    Artist pages

--- site/templates/event.php
+++ site/templates/event.php
@@ -4,16 +4,25 @@
 
   <section class="page-title">
     <article>
-      <div class="column full">
+      <div class="column half">
         <h1><?= $page->title()->html() ?></h1>
+      </div>
+    </article>
+  </section>
+
+  <section class="artist-poster">
+    <article>
+      <div class="column quarter event" style="background-color: <?= $event->color() ?>; background-image: url('<?= $event->poster() ?>')">
       </div>
     </article>
   </section>
 
   <section class="event-info">
     <article>
-      <div class="column full">
-        All the event info should probably go here.
+      <div class="column half">
+        <p>
+          <?= $next->speaker()->bio()->kirbytext() ?>
+        </p>
       </div>
     </article>
   </section>
